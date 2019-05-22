44cc70639cea91035d6aceabf58020da6b6b5d06
Justin <Jefferson@zipcoders-MacBook-Pro-10.local>
2019-05-22 16:30:44 -0400

    Finished Lab

--- Main.java
+++ Main.java
@@ -1,9 +1,44 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args){
+class Main {
+  public static void main(String[] args) {
+    Scanner userIn = new Scanner(System.in); // User Input Object
+    long sum = 0;                            // sum 
+    int counter = 1;                         // counter
+    
+    //Get User input
+    System.out.print("Pick an integer: ");
+    int num = userIn.nextInt();
+
+    // Determine whether to count up or down
+    if (num > 1) {
+
+      while(counter <= num) {
+
+        sum += counter;
+        counter++;
+
+      }
 
     }
+    else if (num < 1) {
+
+      while(counter >= num) {
+
+        sum += counter;
+        counter--;
+
+      }
+
+    } 
+    else {
+
+      sum = 1;
+    }
+
+    //Print Results
+    System.out.println("The sum of the numbers between 1 and " + num + " is " + sum + ".");
+
+
+  }
 }
